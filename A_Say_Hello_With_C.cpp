4249e1c8b84ae34bd4378e12bc3e04b1a75ab6cd
Mostafa Kamal <mostafakamal.cse2022@gmail.com>
2025-11-13 10:19:00 +0600

    Add Solution Problem A

---
+++ A_Say_Hello_With_C.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() {
+    char str[10000];
+    scanf("%s", str);
+    printf("Hello, %s", str);
+    return 0;
+}
+
+/*
+Additional :
+same satement as this Problem.
+
+input :
+get an input as S.
+
+out put :
+Hello, ""
+
+sample:
+input:
+JUST
+
+output
+Hello, "JUST"
+*/
